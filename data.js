0e5b304d787aab4aa40dd607855020987e3cb473
Ramyakrishna124 <144945943+Ramyakrishna124@users.noreply.github.com>
2023-09-13 11:18:42 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+import Cat from './Assests/Cat.png'
+import Flower from './Assests/Flower.png'
+import House from './Assests/House.png'
+
+
+const data = {
+    productItems:[
+        {
+            id: "1",
+            name: "Cat",
+            cost: 400,
+            pic: Cat
+        
+        },
+        {
+            id: "2",
+            name:"Flower",
+            cost:250,
+            pic: Flower
+        },
+        {
+            id: "3",
+            name: "House",
+            cost: 1000,
+            pic: House
+
+        }
+    ]
+
+
+}
+export default data;
